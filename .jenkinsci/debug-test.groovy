4519f76fc655e961a4f84f24291e38ef26c83d60
Artyom Bakhtin <a@bakhtin.net>
2018-07-11 10:39:41 +0300

    fix 12
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- .jenkinsci/debug-test.groovy
+++ .jenkinsci/debug-test.groovy
@@ -1,14 +1,15 @@
 #!/usr/bin/env groovy
 
 def doDebugTest() {
+  // TODO: use docker containers here
   sh """
     docker run -d -t \
       -e POSTGRES_USER=${env.IROHA_POSTGRES_USER} \
       -e POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD} \
       --name ${env.IROHA_POSTGRES_HOST} \
-      --ports 127.0.0.1:5432:5432 postgres:9.5
+      -p 127.0.0.1:5432:5432 postgres:9.5
   """
-  sh "mv $WS_DIR/build/shared_libs/* /usr/lib/$CROSS_TRIPLE_PREFIX"
+  sh "mv $WS_DIR/build/shared_libs/* /usr/lib/aarch64-linux-gnu/"
   sh """
     cd build; ctest --output-on-failure
   """
