07a7eef6d4a692f46fb80dbdc0ea0db906883ca2
Artyom Bakhtin <a@bakhtin.net>
2018-07-11 13:20:06 +0300

    docker test image
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- .jenkinsci/debug-test.groovy
+++ .jenkinsci/debug-test.groovy
@@ -1,18 +1,22 @@
 #!/usr/bin/env groovy
 
 def doDebugTest() {
-  // TODO: use docker containers here
-  sh """
-    docker run -d -t \
-      -e POSTGRES_USER=${env.IROHA_POSTGRES_USER} \
-      -e POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD} \
-      --name ${env.IROHA_POSTGRES_HOST} \
-      -p 127.0.0.1:5432:5432 postgres:9.5
-  """
-  sh "sudo cp $WS_DIR/build/shared_libs/* /usr/lib/aarch64-linux-gnu/"
-  sh """
-    cd build; IROHA_POSTGRES_HOST=127.0.0.1 IROHA_POSTGRES_PORT=5432 ctest --output-on-failure -j4
-  """
+  docker.image('postgres:9.5').withRun(""
+  + " -e POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+  + " -e POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+  + " --name ${env.IROHA_POSTGRES_HOST}"
+  + " --network=${env.IROHA_NETWORK}") {
+    docker.image('local/linux-test-env').inside(""
+	+ " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
+	+ " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
+	+ " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
+	+ " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+	+ " --network=${env.IROHA_NETWORK}"
+	+ " -v ${WS_DIR}:${WS_DIR}") {
+	  sh "cp $WS_DIR/build/shared_libs/* /usr/lib/"
+	  sh "cd build; ctest --output-on-failure"
+	}
+  }
 }
 
 return this
