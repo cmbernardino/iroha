af5dfd0b68a40245aead163298067a7741319fee
Artyom Bakhtin <a@bakhtin.net>
2018-07-13 17:53:32 +0300

    test xunit
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- .jenkinsci/debug-test.groovy
+++ .jenkinsci/debug-test.groovy
@@ -12,7 +12,8 @@ def doDebugTest(workspace) {
 		+ " -e IROHA_POSTGRES_HOST=${env.IROHA_POSTGRES_HOST}"
 		+ " -e IROHA_POSTGRES_PORT=${env.IROHA_POSTGRES_PORT}"
 		+ " -e IROHA_POSTGRES_USER=${env.IROHA_POSTGRES_USER}"
-		+ " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+		//+ " -e IROHA_POSTGRES_PASSWORD=${env.IROHA_POSTGRES_PASSWORD}"
+		+ " -e IROHA_POSTGRES_PASSWORD=123"
 		+ " --network=${env.IROHA_NETWORK}") {
 		    sh "sudo cp -r ${workspace}/build/shared_libs/* /usr/lib/"
 		  	sh "cd build; ctest --output-on-failure --no-compress-output -T Test || true"
@@ -28,6 +29,7 @@ def doDebugTest(workspace) {
 	}
 	finally {
       def currentResult = currentBuild.result ?: 'SUCCESS'
+      sh("docker network rm ${env.IROHA_NETWORK}")
 	}
 
 }
